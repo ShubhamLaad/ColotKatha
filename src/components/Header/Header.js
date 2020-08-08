5ea582eef7d075ef38d0039d75e6f3752764c163
ShubhamLaad <shubhamlaad666@gmail.com>
2020-08-09 01:22:29 +0530

    Header change

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,11 +2,11 @@ import React, { useState } from 'react';
 import './Header.scss'
 import { Container } from '../Layout/Layout';
 import { NavLink, Link } from 'react-router-dom';
+import OutSideCilck from '../OutSideClick/OutSideClick'
 
 const LINKS = [
   { path: '/about', label: 'About Us' },
   { path: '/blogs', label: 'Blogs' },
-
 ]
 
 export default function Header(props) {
@@ -16,7 +16,7 @@ export default function Header(props) {
   return (
     <header>
       <Container>
-        <div className="mobile-wrap">
+        <OutSideCilck className="mobile-wrap" onClose={() => { setshowNav(false) }}>
           <Link to="/" className="logo" title="color katha"><img className="img-responsive" src="/logo.png" alt="logo" /></Link >
           <button className="visible-xs navbar-toggle collapsed" data-toggle="collapse" onClick={() => { setshowNav(!showNav) }}>
             <span className="sr-only d-none">Toggle navigation</span>
@@ -24,7 +24,7 @@ export default function Header(props) {
             <span className="icon-bar"></span>
             <span className="icon-bar"></span>
           </button>
-        </div>
+        </OutSideCilck>
         <ul className={`nav-list ${showNav ? 'show-animation' : ''} `}>
           {
             LINKS.map(link => <li key={link.label}><NavLink to={link.path} title={link.label} activeClassName="active">{link.label}</NavLink ></li>)
